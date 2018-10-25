3bbec3336bb4c86839908053bf5ad19e5f5d7963
David Fu <lexuszhi1990@gmail.com>
2018-10-25 14:45:55 +0800

    add supervisor config

---
+++ install_supervisor.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+sudo apt-get install supervisor
+sudo service supervisor status | grep active
+
+ecoh "update supervisor config"
+
+cat << EOF | sudo tee -a /etc/supervisor/supervisord.conf
+; supervisor config file
+
+[unix_http_server]
+file=/var/run/supervisor.sock   ; (the path to the socket file)
+chmod=0700                       ; sockef file mode (default 0700)
+
+[supervisord]
+logfile=/var/log/supervisor/supervisord.log ; (main log file;default $CWD/supervisord.log)
+pidfile=/var/run/supervisord.pid ; (supervisord pidfile;default supervisord.pid)
+childlogdir=/var/log/supervisor            ; ('AUTO' child log dir, default $TEMP)
+
+; the below section must remain in the config file for RPC
+; (supervisorctl/web interface) to work, additional interfaces may be
+; added by defining them in separate rpcinterface: sections
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+
+[inet_http_server]
+port = *:9090
+username = username
+password = passwd
+
+[supervisorctl]
+serverurl=unix:///var/run/supervisor.sock ; use a unix:// URL  for a unix socket
+
+; The [include] section can just contain the "files" setting.  This
+; setting can list multiple files (separated by whitespace or
+; newlines).  It can also contain wildcards.  The filenames are
+; interpreted as relative to this file.  Included files *cannot*
+; include files themselves.
+
+[include]
+files = /etc/supervisor/conf.d/*.conf
+
+EOF
+
+sudo service supervisor force-reload
+sudo service supervisor status | grep active
+
+ecoh "successed to install supervisor ..."
